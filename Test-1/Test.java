ce202c75ffff9348675f11f780e877e34c3b95b4
Aftab Alam <alamaftab413@gmail.com>
2024-07-03 17:00:16 +0500

    Update Test.java

--- Test-1/Test.java
+++ Test-1/Test.java
@@ -1,5 +1,31 @@
 public class Test{
     public static void main(String[] args) {
-        System.err.println("Hello World");
+        Pizza pizza = new Pizza();
+
+        pizza.setName("Italian");
+        pizza.setPrize(20);
+        System.err.println("Pizza name: "+pizza.getName());
+        System.err.println("Pizza prize: "+pizza.getPrize());
+
     }
-}
+};
+
+class Pizza{
+    private String name;
+    private int prize;
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setPrize(int prize){
+        this.prize=prize;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPrize(){
+        return this.prize;
+    }
+} 
