823e84623d18a55a1649b2268842749349f426cf
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-07-14 04:32:19 +0500

    .

---
+++ sample-codes/Dice/DemoDice.java
@@ -0,0 +1,9 @@
+public class DemoDice{
+	public static void main(String[] args){
+		Dice dice=new Dice();
+		dice.setSides(5);
+
+		System.out.println(dice.getSides());
+	}
+}
+
---
+++ sample-codes/Dice/Dice.java
@@ -0,0 +1,13 @@
+public class Dice {
+	private int sides;
+	private int value;
+
+	public void setSides(int s){
+
+		sides=s;
+	}
+
+	public int getSides(){
+		return sides;
+	}
+}
