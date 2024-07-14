118c724ca4764666ebda7ead6e3f1b8f1254e366
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-07-14 16:55:23 +0500

    Praccticed Makeing saveral Classes

--- sample-codes/Dice/DemoDice.java
+++ sample-codes/Dice/DemoDice.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class DemoDice{
 	public static void main(String[] args){
+		boolean keepRolling=true;
+		Scanner kb= new Scanner(System.in);
 		Dice dice=new Dice();
-		dice.setSides(5);
+		System.out.println("Enter the sides of the Dice");
+		dice.setSides(kb.nextInt());
 
-		System.out.println(dice.getSides());
+		kb.nextLine();
+
+
+
+		do{
+			System.out.print("\n\n=================\nPress enter to roll again \nOR\nPress any other alphanumeric key and press enter to terminate the program: ");
+			
+			if (kb.nextLine()==""){
+				System.out.println("Rolling Dice");
+				System.out.print("\n.\n.\n.\n.\n");
+
+				System.out.print("It rolled up a:\n___\n|"+dice.rollDice()+"|\n---");
+			}
+			else{
+				keepRolling=false;
+				System.out.println("\n\n=======================================\n                   Bye\n=======================================\n\n");
+			}
+			
+		}
+		while (keepRolling);
 	}
 }
 
--- sample-codes/Dice/Dice.java
+++ sample-codes/Dice/Dice.java
@@ -1,6 +1,8 @@
+import java.util.Random;
 public class Dice {
 	private int sides;
 	private int value;
+
 
 	public void setSides(int s){
 
@@ -9,5 +11,10 @@ public class Dice {
 
 	public int getSides(){
 		return sides;
+	}
+
+	public int rollDice(){
+		Random rand = new Random();
+		return rand.nextInt(sides) +1;
 	}
 }
