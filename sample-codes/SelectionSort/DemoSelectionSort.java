72f9ad7eccabcd8a58c8525da65b62a926dee838
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-07-18 23:24:20 +0500

    Partially added selection sort algorithm

---
+++ sample-codes/SelectionSort/DemoSelectionSort.java
@@ -0,0 +1,12 @@
+public class DemoSelectionSort{
+    public static void main(String[] args){
+        int[] array= {3,6,23,7,1,3,7};
+
+        SelectionSort ss= new SelectionSort(array);
+        array=ss.sort();
+
+        // for (int i=0; i<array.length; i++){
+        //     System.out.print(array[i]+", ");
+        // }
+    }
+}
