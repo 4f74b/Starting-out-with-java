1cde225bd822e3c46cfd26ddd0340df03a1babce
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-07-26 17:39:05 +0500

    Created a java classs that takes a two dimensional array as input and figures out whether it is lo shu Square or not

--- sample-codes/LoShuMagic/DemoLoShuMagic.java
+++ sample-codes/LoShuMagic/DemoLoShuMagic.java
@@ -1,7 +1,13 @@
 public class DemoLoShuMagic {
     public static void main(String[] args) {
-        int[][] arr= {{12,4,2},{4,6,3},{6,7,3}};
+        int[][] arr= {{4,9,2},{3,5,7},{8,1,6}};
         LoShuMagic ls= new LoShuMagic();
-        ls.check(arr);
+        boolean isLoShu= ls.check(arr);
+        if(isLoShu){
+            System.out.println("The array is lo Shu");
+        }
+        else{
+            System.out.println("Not Lo Shu");
+        }
     }
 }
--- sample-codes/LoShuMagic/LoShuMagic.java
+++ sample-codes/LoShuMagic/LoShuMagic.java
@@ -1,17 +1,27 @@
 public class LoShuMagic {
     public boolean check(int[][] array){
         boolean isLoShu=true;
-        int acc;
+        int sum;
         // iterate through rows
         for (int row=0; row< array.length; row++){
-            acc=0;
+            sum=0;
             // iterate through elements of a column and add them up, at the end of loop check of sum is 15, if it isn't make isLoShu false
             for (int col=0; col< array[row].length && isLoShu; col++){
-                acc+=array[row][col];
+                sum+=array[row][col];
             }
-            if(acc!=15){
+            if(sum!=15){
                 isLoShu=false;
             }
+        }
+
+        // iterate through cols
+        for (int col=0; col<array[0].length; col++){
+            sum=0;
+            for (int row=0; row<array.length; row++){
+                sum+=array[row][col];
+            }
+            if (sum!=15)
+                isLoShu=false;
         }
         return isLoShu;
     }
