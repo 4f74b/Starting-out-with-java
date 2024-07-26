e9b385f597711ad6a7748e949e4dc69ec85e3205
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-07-27 00:09:15 +0500

    partially developed Binary Search

--- sample-codes/BinarySearchAlgo/BinarySearch.java
+++ sample-codes/BinarySearchAlgo/BinarySearch.java
@@ -1,7 +1,35 @@
 public class BinarySearch {
 
-    public int bs(int item){
+    public static int bs(int[] arr,int item){
+        // run a loop unless the element is found or unless the array can not be search anymore because element is not found
+        boolean itemFound=false, noMoreEls=false;
+        // set index such that it points to the middle element of the array
+        int index=(int)Math.floor(arr.length/2);
+        while(!itemFound && !noMoreEls){
+            // Check whether the element is equal to the center element or less than it or greater
+            System.out.println("index : "+index+" : El : "+arr[index]+" : "+item);
+            if (arr[index]==item){
+                itemFound=true;
+            }
+            // Check if index is equal to 0, if so, set noMoreEls to zero
+            if(index==0 || index==arr.length-1){
+                noMoreEls=true;
+                break;
+            }
+            else if (item<arr[index]){
+                index-=(int)Math.floor(index/2);
+            }
+            else if (item >arr[index]){
+                index+=(int)Math.floor(index/2);
+            }
+            
 
+        }
+        if (itemFound){
+            return index;
+        }else{
+            return -1;
+        }
     }
 
    
--- sample-codes/BinarySearchAlgo/DemoBinarySearch.java
+++ sample-codes/BinarySearchAlgo/DemoBinarySearch.java
@@ -1,12 +1,15 @@
 public class DemoBinarySearch {
     public static void main(String[] args) {
-        int[] array = { 3, 4, 2, 15, 1, 7, 8,23,23,12,6,2,6,223,2 };
+        int[] array = { 3, 4, 2, 15, 1, 7, 8, 23, 23, 12, 6, 2, 6, 223, 2 };
 
         // First Sort the array
         SelectionSort.ss(array);
 
-        for (int el: array){
-            System.out.println(el);
+        for (int el : array) {
+            System.out.print(el+", ");
         }
+        System.out.println();
+
+        System.out.println(BinarySearch.bs(array, 15));
     }
 }
