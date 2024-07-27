36cdb0d2fba41b0734ef583262a48ee25ba80243
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-07-27 16:27:35 +0500

    Completed Binary searcch Algorithm

--- sample-codes/BinarySearchAlgo/BinarySearch.java
+++ sample-codes/BinarySearchAlgo/BinarySearch.java
@@ -3,30 +3,30 @@ public class BinarySearch {
     public static int bs(int[] arr,int item){
         // run a loop unless the element is found or unless the array can not be search anymore because element is not found
         boolean itemFound=false, noMoreEls=false;
-        // set index such that it points to the middle element of the array
-        int index=(int)Math.floor(arr.length/2);
-        while(!itemFound && !noMoreEls){
-            // Check whether the element is equal to the center element or less than it or greater
-            System.out.println("index : "+index+" : El : "+arr[index]+" : "+item);
-            if (arr[index]==item){
-                itemFound=true;
-            }
-            // Check if index is equal to 0, if so, set noMoreEls to zero
-            if(index==0 || index==arr.length-1){
-                noMoreEls=true;
-                break;
-            }
-            else if (item<arr[index]){
-                index-=(int)Math.floor(index/2);
-            }
-            else if (item >arr[index]){
-                index+=(int)Math.floor(index/2);
-            }
+        // set mid variable such that it points to the middle element of the array
+        int lowerBound=0, upperBound=arr.length-1,mid=(int)Math.floor((lowerBound+upperBound)/2);
+        while (itemFound==false && noMoreEls==false){
             
-
+            if(arr[mid]==item){
+                itemFound=true;
+                System.out.println("element found");
+            }
+            // after comparison, if upper bound is one more that lower bound, then break the loop
+            if(upperBound==lowerBound+1){
+                noMoreEls=true;
+            }
+            if (item>arr[mid]){
+                lowerBound=mid;
+                mid+=(int)Math.floor((upperBound-lowerBound)/2);
+            }
+            else if (item < arr[mid]){
+                upperBound=mid;
+                mid-=(int)Math.floor((upperBound-lowerBound)/2);
+            }
+            // System.out.println("upperbound: "+upperBound+", LowerBound: "+lowerBound+", Mid: "+mid);
         }
         if (itemFound){
-            return index;
+            return mid;
         }else{
             return -1;
         }
--- sample-codes/BinarySearchAlgo/DemoBinarySearch.java
+++ sample-codes/BinarySearchAlgo/DemoBinarySearch.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
+
 public class DemoBinarySearch {
     public static void main(String[] args) {
-        int[] array = { 3, 4, 2, 15, 1, 7, 8, 23, 23, 12, 6, 2, 6, 223, 2 };
+        int[] array = { 3, 4, 15, 1, 7, 8, 23, 12, 6, 223, 2 };
+        int item, res;
+        Scanner kb = new Scanner(System.in);
 
         // First Sort the array
         SelectionSort.ss(array);
 
+        System.out.println("This is the array");
         for (int el : array) {
             System.out.print(el+", ");
         }
         System.out.println();
 
-        System.out.println(BinarySearch.bs(array, 15));
+
+        System.out.print("Enter Element that you want to find: ");
+        item=kb.nextInt();
+
+        res=BinarySearch.bs(array, item);
+        if (res==-1){
+            System.out.println(item+" was not present in the array");
+        }
+        else{
+            System.out.println("Item was found at index : "+res+" of the array");
+        }
+
     }
 }
