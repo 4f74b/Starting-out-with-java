f2627f08356b99793f29d4db8024759926a7e881
Aftab Alam <101344790+4f74b@users.noreply.github.com>
2024-12-04 18:59:23 +0500

    .

---
+++ Spring Boot/Implements/noob-1/src/test/java/com/aftab/noob_1/Noob1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aftab.noob_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Noob1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
